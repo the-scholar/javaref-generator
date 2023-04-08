60ca837b9fd7a3e076eae27f0eaae44121d1e048
the-scholar <the-scholar@dusttoash.org>
2023-04-08 01:26:38 -0500

    Update SyntaxPage

--- src/scholar/generators/javaref/api/SyntaxPage.java
+++ src/scholar/generators/javaref/api/SyntaxPage.java
@@ -2,27 +2,40 @@ package scholar.generators.javaref.api;
 
 public class SyntaxPage extends Page {
 
-	/**
-	 * The title of the page. This is used both for the page
-	 * <code>&lt;title&gt;</code> element, as well as for the top-level header
-	 * (<code>&lt;h1&gt;</code>) that begins the article.
-	 */
-	private final String title;
+	private final SectionHeader topLevelSection;
 	/**
 	 * An OPTIONAL, short-and-sweet description of the topic. This goes immediately
 	 * under the {@link #title} and usually renders italicized. It gives a very
 	 * brief definition/description of what the concept that is the focus of the
 	 * page, is.
 	 */
-	private String pageDescription;
+	private Paragraph pageDescription;
+	
 
-	/**
-	 * The contents that goes after the
-	 */
-	private SectionContent titleContent = new SectionContent();
-
-	public SyntaxPage(String location) {
+	public SyntaxPage(String location, String title, String pageDescription) {
 		super(location);
+		topLevelSection = new SectionHeader(title, 1);
+	}
+
+	public String getTitle() {
+		return topLevelSection.getTitle();
+	}
+
+	public void setTitle(String title) {
+		topLevelSection.setTitle(title);
+	}
+
+	public String getPageDescription() {
+		return pageDescription.getContent();
+	}
+
+	public void setPageDescription(String description) {
+		pageDescription = description == null || description.isEmpty() ? null : new Paragraph(description);
+	}
+
+	@Override
+	public String print() {
+		return "<div id=\"Content\">" + Element.print(topLevelSection, pageDescription);
 	}
 
 }
