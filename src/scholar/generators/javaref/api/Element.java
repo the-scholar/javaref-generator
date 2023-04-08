7bb1eaeeb78f8347de31118da9ff864c7bd9a112
the-scholar <the-scholar@dusttoash.org>
2023-04-08 00:02:03 -0500

    Update Element print functions

--- src/scholar/generators/javaref/api/Element.java
+++ src/scholar/generators/javaref/api/Element.java
@@ -9,12 +9,22 @@ public interface Element {
 		return print("", elements);
 	}
 
+	/**
+	 * Prints all of the provided {@link Element}s, in order, with the specified
+	 * delimiter between them. This method ignores any <code>null</code>
+	 * {@link Element}s, for convenience.
+	 * 
+	 * @param delimiter The delimiter to print between each {@link Element}.
+	 * @param elements  The array of {@link Element}s to print.
+	 * @return The resulting {@link String}.
+	 */
 	static String print(String delimiter, Element... elements) {
 		if (elements == null || elements.length == 0)
 			return "";
-		StringBuilder sb = new StringBuilder(elements[0].print());
+		StringBuilder sb = new StringBuilder(elements[0] == null ? "" : elements[0].print());
 		for (var v : elements)
-			sb.append(delimiter).append(v.print());
+			if (v != null)
+				sb.append(delimiter).append(v.print());
 		return sb.toString();
 	}
 
@@ -22,14 +32,33 @@ public interface Element {
 		return print("", elements);
 	}
 
+	/**
+	 * Prints all of the provided {@link Element}s, in order, with the specified
+	 * delimiter between them. This method ignores any <code>null</code>
+	 * {@link Element}s, for convenience.
+	 * 
+	 * @param delimiter The delimiter to print between each {@link Element}.
+	 * @param elements  The {@link Iterator} of {@link Element}s to print.
+	 * @return The resulting {@link String}.
+	 */
 	static String print(String delimiter, Iterator<? extends Element> elements) {
 		if (elements == null || !elements.hasNext())
 			return "";
 
-		StringBuilder sb = new StringBuilder(elements.next().print());
+		Element e = elements.next();
+		StringBuilder sb = new StringBuilder(e == null ? "" : e.print());
 		while (elements.hasNext())
-			sb.append(delimiter).append(elements.next().print());
+			if ((e = elements.next()) != null)
+				sb.append(delimiter).append(e.print());
 		return sb.toString();
+	}
+
+	static String print(String delimiter, Iterable<? extends Element> elements) {
+		return print(delimiter, elements.iterator());
+	}
+
+	static String print(Iterable<? extends Element> elements) {
+		return print("", elements);
 	}
 
 }
