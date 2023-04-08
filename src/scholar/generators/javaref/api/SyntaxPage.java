ddf2af31b1ac4e5cc8caf9ef456cb8ea72ca8432
the-scholar <the-scholar@dusttoash.org>
2023-04-07 23:23:30 -0500

    Update SyntaxPage

--- src/scholar/generators/javaref/api/SyntaxPage.java
+++ src/scholar/generators/javaref/api/SyntaxPage.java
@@ -2,6 +2,25 @@ package scholar.generators.javaref.api;
 
 public class SyntaxPage extends Page {
 
+	/**
+	 * The title of the page. This is used both for the page
+	 * <code>&lt;title&gt;</code> element, as well as for the top-level header
+	 * (<code>&lt;h1&gt;</code>) that begins the article.
+	 */
+	private final String title;
+	/**
+	 * An OPTIONAL, short-and-sweet description of the topic. This goes immediately
+	 * under the {@link #title} and usually renders italicized. It gives a very
+	 * brief definition/description of what the concept that is the focus of the
+	 * page, is.
+	 */
+	private String pageDescription;
+
+	/**
+	 * The contents that goes after the
+	 */
+	private SectionContent titleContent = new SectionContent();
+
 	public SyntaxPage(String location) {
 		super(location);
 	}
