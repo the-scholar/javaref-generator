908412c45bd4f06a656ae0ae48366d48e739e28f
the-scholar <the-scholar@dusttoash.org>
2023-04-07 23:09:05 -0500

    Add Page class

---
+++ src/scholar/generators/javaref/api/Page.java
@@ -0,0 +1,18 @@
+package scholar.generators.javaref.api;
+
+public class Page {
+	private String location;
+
+	public Page(String location) {
+		this.location = location;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
